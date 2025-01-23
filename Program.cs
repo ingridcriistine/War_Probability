4ef296038b2c317e2ba6ed6f2a0ff10600daeb88
ingridcriistine <ingridrochacristine@gmail.com>
2025-01-23 16:33:02 -0300

    primeira parte

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime;
+
+Random rand = new Random();
+bool game = true;
+Soldado atacantes = new(1000);
+Soldado defensores = new(500);
+
+void War(Soldado atacantes, Soldado defensores) {
+
+    List<int> dadosAtaque = [];
+    List<int> dadosDefesa = [];
+
+    for (int i = 0; i < (atacantes.qtdSoldados < 4 ? atacantes.qtdSoldados - 1 : 3); i++)
+        dadosAtaque.Add(rand.Next(6) + 1);
+
+    for (int i = 0; i < (defensores.qtdSoldados < 4 ? defensores.qtdSoldados : 3); i++)
+        dadosDefesa.Add(rand.Next(6) + 1);
+
+    dadosAtaque.Sort();
+    dadosAtaque.Reverse();
+    dadosDefesa.Sort();
+    dadosDefesa.Reverse();
+
+    // Console.WriteLine("Ataque");
+    // for (int i = 0; i < dadosAtaque.Count(); i++)
+    // {
+    //     Console.WriteLine(dadosAtaque[i]);
+    // }
+
+    // Console.WriteLine("Defesa");
+    // for (int i = 0; i < dadosDefesa.Count(); i++)
+    // {
+    //     Console.WriteLine(dadosDefesa[i]);
+    // }
+
+    for (int i = 0; i < (dadosAtaque.Count() < dadosDefesa.Count() ? dadosAtaque.Count() : dadosDefesa.Count()); i++)
+    {
+        if(dadosAtaque[i] > dadosDefesa[i])
+            defensores.qtdSoldados -= 1;
+        else
+            atacantes.qtdSoldados -= 1;
+    }
+}
+
+while(game) {
+    War(atacantes, defensores);
+    if(atacantes.qtdSoldados <= 1 || defensores.qtdSoldados == 0) 
+        game = false;
+}
+
+Console.WriteLine($"Nº Atacantes: {atacantes.qtdSoldados}");
+Console.WriteLine($"Nº Defensores: {defensores.qtdSoldados}");
---
+++ Soldado.cs
@@ -0,0 +1,4 @@
+public class Soldado(int qtdSoldados) {
+
+    public int qtdSoldados { get; set; } = qtdSoldados;
+}
